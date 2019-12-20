d9d8d75f45c0ee72aef05cc4212bc3a0de44c841
Martin Barri <maeertin@gmail.com>
2019-12-20 14:58:51 +0100

    [storybook] Configure @storybook/addon-a11y

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { addDecorator, configure } from '@storybook/react'
+import { withA11y } from '@storybook/addon-a11y'
 import { withKnobs, select } from '@storybook/addon-knobs'
 import MuiThemeProvider from '@material-ui/styles/ThemeProvider'
 import CssBaseline from '@material-ui/core/CssBaseline'
@@ -30,5 +31,6 @@ addDecorator(storyFn => {
   )
 })
 addDecorator(withKnobs)
+addDecorator(withA11y)
 
 configure(loadStories, module)
