1f508acdc82e26230f01e7ac4a6d22bb1028a406
Martin Barri <maeertin@gmail.com>
2020-01-15 10:40:45 +0100

    [_app] Add descriptive comment

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,6 +9,7 @@ import AppBase from 'containers/App'
 
 export default class MyApp extends App {
   componentDidMount() {
+    // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side')
     if (jssStyles) {
       jssStyles.parentNode.removeChild(jssStyles)
