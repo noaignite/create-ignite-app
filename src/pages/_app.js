86a5dcf9b70c9e656bfe26c97f10ebf0970725ea
Vincent Boiardt <vincent@boiardt.se>
2020-01-21 16:35:53 +0100

    [_app] Refactor to functional component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,27 +1,32 @@
 // based on https://github.com/mui-org/material-ui/blob/master/examples/nextjs/pages/_app.js
 
 import React from 'react'
-import App from 'next/app'
 import { ThemeProvider } from '@material-ui/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import theme from 'src/theme.default'
 import AppBase from 'containers/App'
 
-export default class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props
+const App = ({ Component, pageProps }) => {
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
 
-    return (
-      <React.Fragment>
-        <ThemeProvider theme={theme}>
-          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-          <CssBaseline />
+  return (
+    <React.Fragment>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
 
-          <AppBase>
-            <Component {...pageProps} />
-          </AppBase>
-        </ThemeProvider>
-      </React.Fragment>
-    )
-  }
+        <AppBase>
+          <Component {...pageProps} />
+        </AppBase>
+      </ThemeProvider>
+    </React.Fragment>
+  )
 }
+
+export default App
