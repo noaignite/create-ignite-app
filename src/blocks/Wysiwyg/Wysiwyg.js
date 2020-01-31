3aa2cebfed4398bf5906f53283659990915a2933
Martin Barri <maeertin@gmail.com>
2020-01-31 15:53:22 +0100

    [Wysiwyg] Add content prop as a children alternative for when data is passed as a string

--- src/blocks/Wysiwyg/Wysiwyg.js
+++ src/blocks/Wysiwyg/Wysiwyg.js
@@ -5,17 +5,27 @@ import Html from 'components/Html'
 import Section from 'components/Section'
 
 const Wysiwyg = React.forwardRef(function Wysiwyg(props, ref) {
-  const { children, ...other } = props
+  const { children, content, ...other } = props
+
+  const componentProps = {}
+  if (content) {
+    componentProps.dangerouslySetInnerHTML = { __html: content }
+  } else {
+    componentProps.children = children
+  }
 
   return (
     <Section ref={ref} {...other}>
-      <Container component={Html}>{children}</Container>
+      <Container>
+        <Html {...componentProps} />
+      </Container>
     </Section>
   )
 })
 
 Wysiwyg.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node,
+  content: PropTypes.string,
 }
 
 Wysiwyg.uiName = 'Block/Wysiwyg'
