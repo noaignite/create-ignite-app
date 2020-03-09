51c5a893976718506b55ccb8f61d6c3a4a42fce1
Martin Barri <maeertin@gmail.com>
2020-03-09 16:22:09 +0100

    [Slideshow] Update according to @oakwood/oui@0.8.1

--- src/components/Slideshow/Slideshow.js
+++ src/components/Slideshow/Slideshow.js
@@ -1,1 +1,11 @@
-export { default } from '@oakwood/oui/Rwiper'
+import * as React from 'react'
+import Rwiper from '@oakwood/oui/Rwiper'
+import { Swiper } from 'swiper/js/swiper.esm'
+
+const Slideshow = React.forwardRef(function Slideshow(props, ref) {
+  return <Rwiper Swiper={Swiper} ref={ref} {...props} />
+})
+
+Slideshow.uiName = 'Slideshow'
+
+export default Slideshow
--- src/components/Slideshow/stories.js
+++ src/components/Slideshow/stories.js
@@ -1,8 +1,7 @@
-import React from 'react'
+import * as React from 'react'
 import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
-import { A11y, Navigation, Pagination } from 'swiper/js/swiper.esm'
 import Media from '@oakwood/oui/Media'
+import { A11y, Keyboard, Navigation, Pagination } from 'swiper/js/swiper.esm'
 import SlideshowNavigation from '../SlideshowNavigation'
 import SlideshowPagination from '../SlideshowPagination'
 import SlideshowSlide from '../SlideshowSlide'
@@ -10,24 +9,18 @@ import Slideshow from './Slideshow'
 
 const stories = storiesOf('Components/Slideshow', module)
 
-stories.add('Default', () => (
+export const Default = () => (
   <Slideshow
-    modules={[A11y, Navigation, Pagination]}
+    modules={[A11y, Keyboard, Navigation, Pagination]}
     navigation={{
-      prevEl: <SlideshowNavigation variant="prev" aria-label="Previous Slide" />,
-      nextEl: <SlideshowNavigation variant="next" aria-label="Next Slide" />,
+      prevEl: <SlideshowNavigation variant="previous" />,
+      nextEl: <SlideshowNavigation variant="next" />,
     }}
     pagination={{
       el: <SlideshowPagination />,
       clickable: true,
     }}
-    on={{
-      init: action('init'),
-      slideChange: action('slideChange'),
-      // Register desired swiper event handlers...
-    }}
-    watchOverflow
-    // For more settings visit http://idangero.us/swiper/api/
+    keyboard
   >
     {Array.from(new Array(6), (val, idx) => (
       <SlideshowSlide key={idx}>
@@ -35,43 +28,8 @@ stories.add('Default', () => (
       </SlideshowSlide>
     ))}
   </Slideshow>
-))
+)
 
-stories.add('External Controls', () => {
-  const navigationPrevRef = React.useRef(null)
-  const navigationNextRef = React.useRef(null)
-  const paginationRef = React.useRef(null)
-
-  return (
-    <>
-      <Slideshow
-        navigation={{
-          prevEl: () => navigationPrevRef.current,
-          nextEl: () => navigationNextRef.current,
-        }}
-        pagination={{
-          el: () => paginationRef.current,
-          clickable: true,
-        }}
-      >
-        {Array.from(new Array(6), (val, idx) => (
-          <SlideshowSlide key={idx}>
-            <Media component="img" src={`//placekitten.com/800/${400 - idx}`} />
-          </SlideshowSlide>
-        ))}
-      </Slideshow>
-
-      <div>
-        <button type="button" ref={navigationPrevRef}>
-          Prev
-        </button>
-        <button type="button" ref={navigationNextRef}>
-          Next
-        </button>
-        <div ref={paginationRef} />
-      </div>
-    </>
-  )
-})
+stories.add('Default', Default)
 
 export default Slideshow
