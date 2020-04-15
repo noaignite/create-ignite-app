29f00935c77740c2b29e76221212b4e7c7f1b04f
Martin Barri <maeertin@gmail.com>
2020-04-15 11:47:58 +0200

    [theme] Simplify colors by exporting Mui pre-defined ones. Update theme palette with missing action vars and set up local definitions of warning & success.

--- src/components/colors/orange.js
+++ src/components/colors/orange.js
@@ -2,4 +2,4 @@
  * Replace export with custom color definition
  * if in need of changing the color scale.
  */
-export { default } from '@material-ui/core/colors/red'
+export { default } from '@material-ui/core/colors/orange'
