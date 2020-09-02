cd9beb6a351de2edf6e57d8df453904c8eb3bf5a
Martin Barri <maeertin@gmail.com>
2020-09-02 18:36:39 +0200

    [App] Remove App HOCs

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,7 +1,1 @@
-// import { compose } from 'recompose'
-// import { withCmsContext } from 'api'
-// import { withAppContextProvider } from './AppContext'
-// import App from './App'
-
-// export default compose(withCmsContext, withAppContextProvider)(App)
 export { default } from './App'
