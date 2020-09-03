b7dc64f63937560bea0fe2a35c18941eefa3470e
Martin Barri <maeertin@gmail.com>
2020-09-03 16:37:00 +0200

    [blocks] Add Media block to common exports

--- src/blocks/index.js
+++ src/blocks/index.js
@@ -1,3 +1,4 @@
 export { default as Content } from './Content'
 export { default as Hero } from './Hero'
+export { default as Media } from './Media'
 export { default as ProductSlideshow } from './ProductSlideshow'
