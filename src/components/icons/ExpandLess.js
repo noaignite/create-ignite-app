97c39254f735c7932f5828b20155cc11e5512fde
Martin Barri <maeertin@gmail.com>
2020-09-09 14:01:32 +0200

    [icons] Update expand icons

--- src/components/icons/ExpandLess.js
+++ src/components/icons/ExpandLess.js
@@ -2,6 +2,6 @@ import * as React from 'react'
 import createSvgIcon from '../utils/createSvgIcon'
 
 export default createSvgIcon(
-  <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" />,
-  'ExpandMore',
+  <path d="M12 8l-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14z" />,
+  'ExpandLess',
 )
