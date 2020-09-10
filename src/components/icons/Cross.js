6a534fd09200342f18e5d72dafcb61b8201b4a2c
Martin Barri <maeertin@gmail.com>
2020-09-10 17:34:21 +0200

    [icons] Rename old icons and add new that are commonly used

--- src/components/icons/Cross.js
+++
@@ -1,7 +0,0 @@
-import * as React from 'react'
-import createSvgIcon from '../utils/createSvgIcon'
-
-export default createSvgIcon(
-  <polygon points="19.159 5.371 18.629 4.841 12 11.47 5.371 4.841 4.841 5.371 11.47 12 4.841 18.629 5.371 19.159 12 12.53 18.629 19.159 19.159 18.629 12.53 12 19.159 5.371" />,
-  'Cross',
-)
