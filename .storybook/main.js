32bd04f3a5c5c627db6b98dedf4a1c66c6bf79dc
Martin Barri <maeertin@gmail.com>
2020-10-06 18:36:12 +0200

    [storybook] Simplify Storybook deps via @storybook/addon-essentials

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,6 @@
 module.exports = {
   stories: ['../src/**/*stories.js'],
-  addons: [
-    '@storybook/addon-toolbars',
-    '@storybook/addon-controls',
-    '@storybook/addon-actions',
-    '@storybook/addon-a11y',
-  ],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
   webpackFinal: async (config /*, { configType }*/) => {
     // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
     // You can change the configuration based on that.
