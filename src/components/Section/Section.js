4f71768fafc2d984ae653b777c3d3335a94a39f6
Martin Barri <maeertin@gmail.com>
2020-12-01 17:29:49 +0100

    [Section] Small refactor to how spacing is applied and canceled

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -4,15 +4,22 @@ import classnames from 'clsx'
 import withStyles from '@material-ui/core/styles/withStyles'
 
 export const styles = (theme) => ({
-  root: {
+  root: ({ spacingRule = 'margin' }) => ({
+    ...theme.mixins.section(spacingRule),
     position: 'relative',
-  },
+  }),
+  // Applying spacing to root and thereafter canceling via `disableSpacing`
+  // allows for the css variable `--section-spacing` to still be applied for
+  // nested styling use.
+  disableSpacing: ({ spacingRule = 'margin' }) => ({
+    [`${spacingRule}Top`]: 0,
+    [`${spacingRule}Bottom`]: 0,
+  }),
   regular: {
     ...theme.mixins.verticalRhythm(4, '*:not([aria-hidden])'),
     [theme.breakpoints.up('sm')]: theme.mixins.verticalRhythm(6, '*:not([aria-hidden])'),
   },
   dense: theme.mixins.verticalRhythm(2, '*:not([aria-hidden])'),
-  spacing: ({ spacingRule = 'margin' }) => theme.mixins.section(spacingRule),
 })
 
 const Section = React.forwardRef(function Section(props, ref) {
@@ -33,7 +40,7 @@ const Section = React.forwardRef(function Section(props, ref) {
         classes.root,
         {
           [classes[rhythm]]: rhythm !== false,
-          [classes.spacing]: !disableSpacing,
+          [classes.disableSpacing]: disableSpacing,
         },
         className,
       )}
