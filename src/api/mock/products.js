685be542cac1c80c5e0b7c2f5f6cef951f5f24b3
Martin Barri <maeertin@gmail.com>
2020-12-01 15:03:18 +0100

    [api] Update api mock to contain an array of products instead of a single product

--- src/api/mock/products.js
+++ src/api/mock/products.js
@@ -4,15 +4,24 @@ export const productMedia = [
   '//source.unsplash.com/380x560',
 ]
 
-export default {
+const product = {
   media: {
     full: productMedia,
     standard: productMedia,
     thumb: productMedia,
   },
   name: 'Generic Product',
-  price: '20 EUR',
-  priceBeforeDiscount: '30 EUR',
+  price: '19 EUR',
+  priceBeforeDiscount: null,
   stock: 10,
   uri: 'generic-product',
 }
+
+const products = Array.from(new Array(7), (_, idx) => ({
+  ...product,
+  name: `Generic Product ${idx + 1}`,
+  priceBeforeDiscount: idx % 2 === 0 ? '29 EUR' : null,
+  stock: 9 - idx,
+}))
+
+export default products
