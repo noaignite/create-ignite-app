f2ec19da3c71ac79808e74ed13a40d2ce946358b
Dennis Holmén <dennis.holmen@oakwood.se>
2020-12-09 15:29:27 +0100

    [_document] Added preload example for fonts.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -19,6 +19,7 @@ export default class MyDocument extends Document {
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="shortcut icon" href="/favicon.ico" />
           <link rel="manifest" href="/site.webmanifest" />
+          {/* <link rel="preload" href="/fonts/example.woff2" as="font" type="font/woff2" crossOrigin="anonymous" /> */}
         </Head>
         <body>
           <Main />
