169b2069656cf1068974f742f8c649154b2af8c2
Martin Barri <maeertin@gmail.com>
2021-06-23 22:15:40 +0200

    [wdyr] Change conditional to check against production env

--- scripts/wdyr.js
+++ scripts/wdyr.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 
 // Monkey patches React to notify you about avoidable re-renders.
 // Based on: https://github.com/vercel/next.js/tree/canary/examples/with-why-did-you-render
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV !== 'production') {
   if (typeof window !== 'undefined') {
     // eslint-disable-next-line global-require
     const whyDidYouRender = require('@welldone-software/why-did-you-render')
