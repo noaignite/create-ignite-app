c2724a7997982dd3944f530027c6df6d4207b3f9
Martin Barri <maeertin@gmail.com>
2021-06-23 16:25:57 +0200

    [api] Remove unnecessary storybook.index files

--- src/api/I18nContext/index.js
+++ src/api/I18nContext/index.js
@@ -1,3 +1,50 @@
-// @todo Replace mocked exports with real implementation.
-export { default } from './storybook.index'
-export * from './storybook.index'
+import * as React from 'react'
+import PropTypes from 'prop-types'
+import i18n, { i18nConfig } from 'es2015-i18n-tag'
+
+const I18nContext = React.createContext({})
+
+if (process.env.NODE_ENV !== 'production') {
+  I18nContext.displayName = 'I18nContext'
+}
+
+export function useI18n() {
+  return React.useContext(I18nContext)
+}
+
+const DEFAULT_LOCALE = 'en'
+
+export function I18nProvider(props) {
+  const { children, defaultLocale = DEFAULT_LOCALE, locale } = props
+
+  let translations
+  try {
+    // eslint-disable-next-line global-require, import/no-dynamic-require
+    translations = require(`../../../public/locales/${locale}.json`)
+  } catch (err) {
+    // eslint-disable-next-line global-require, import/no-dynamic-require
+    translations = require(`../../../public/locales/${defaultLocale}.json`)
+  }
+
+  i18nConfig({
+    translations,
+    // Intl DateTimeFormat options as described here: https://goo.gl/lslekB
+    date: {},
+    // Intl NumberFormat options as described here: https://goo.gl/pDwbG2
+    number: {},
+  })
+
+  const contextValue = {
+    t: i18n,
+  }
+
+  return <I18nContext.Provider value={contextValue}>{children}</I18nContext.Provider>
+}
+
+I18nProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+  defaultLocale: PropTypes.string,
+  locale: PropTypes.string,
+}
+
+export default I18nContext
