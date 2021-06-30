cdfa2e46607fd4951545a7afc181252e1c5bf929
Martin Barri <maeertin@gmail.com>
2021-06-30 11:05:36 +0200

    [api/mock] Small tweaks, add categories & order mocks

---
+++ src/api/mock/categories.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    category: '1',
+    name: ['New arrivals'],
+    sortOrder: 1,
+    uri: 'new-arrivals',
+  },
+  {
+    category: '2',
+    name: ['Clothing'],
+    sortOrder: 2,
+    uri: 'clothing',
+  },
+  {
+    category: '3',
+    name: ['Accessories'],
+    sortOrder: 3,
+    uri: 'accessories',
+  },
+  {
+    category: '4',
+    name: ['Shoes'],
+    sortOrder: 4,
+    uri: 'shoes',
+  },
+  {
+    category: '5',
+    name: ['Homewear'],
+    sortOrder: 5,
+    uri: 'homewear',
+  },
+  {
+    category: '6',
+    name: ['Skincare'],
+    sortOrder: 6,
+    uri: 'skincare',
+  },
+]
