6a470a3ffbd9cc283b6e62a51f18030c56254771
Martin Barri <maeertin@gmail.com>
2021-09-21 18:28:31 +0200

    [Media] Remove Container

--- src/blocks/Media/Media.js
+++ src/blocks/Media/Media.js
@@ -2,24 +2,21 @@ import * as React from 'react'
 import PropTypes from 'prop-types'
 import { mediaType } from '@noaignite/oui/utils'
 import { Media as OuiMedia, MediaReveal } from '@noaignite/oui'
-import { Container } from 'components'
 
 function Media(props) {
   const { mediaProps, renderIndex } = props
 
   return (
     <section>
-      <Container>
-        <MediaReveal>
-          <OuiMedia
-            {...(mediaProps?.component === 'video'
-              ? { autoPlay: true, muted: true, loop: true, playsInline: true }
-              : {})}
-            {...mediaProps}
-            priority={renderIndex === 0}
-          />
-        </MediaReveal>
-      </Container>
+      <MediaReveal>
+        <OuiMedia
+          {...(mediaProps?.component === 'video'
+            ? { autoPlay: true, muted: true, loop: true, playsInline: true }
+            : {})}
+          {...mediaProps}
+          priority={renderIndex === 0}
+        />
+      </MediaReveal>
     </section>
   )
 }
