fd0ccaba6d25e1faf869ae1a7e0dbea9c7db618c
Martin Barri <maeertin@gmail.com>
2021-10-04 11:17:15 +0200

    [Html] Use collapsing margins with top & bottom margins

--- src/components/Html/Html.js
+++ src/components/Html/Html.js
@@ -13,7 +13,7 @@ export const HtmlRoot = styled('div', {
   h5: theme.typography.h6,
   h6: theme.typography.h6,
   'h1, h2, h3, h4, h5, h6, p, ol, ul, figure, img': {
-    margin: '1.2em auto 0',
+    margin: '1.2em auto 0.6em',
     '&:first-child': {
       marginTop: 0,
     },
