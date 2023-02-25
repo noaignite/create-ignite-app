52667260ace5cea81c497105ccccea898f7f6250
Martin Barri <maeertin@gmail.com>
2023-02-25 10:29:00 +0100

    feat(Storybook/RouterLink): remove SB mocked RouterLink as no longer needed

--- src/containers/RouterLink/storybook.index.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react'
-
-const RouterLink = React.forwardRef((props, ref) => (
-  // eslint-disable-next-line jsx-a11y/anchor-has-content
-  <a ref={ref} {...props} />
-))
-
-export default RouterLink
