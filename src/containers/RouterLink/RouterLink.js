760242653cf1abefa6c292d11bad0aaf0f3f770c
Martin Barri <maeertin@gmail.com>
2023-02-25 10:28:06 +0100

    feat(RouterLink): remove unused custom 'external' prop

--- src/containers/RouterLink/RouterLink.js
+++ src/containers/RouterLink/RouterLink.js
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types'
 import Link from 'next/link'
 
 const RouterLink = React.forwardRef(function RouterLink(props, ref) {
-  const { as, children, external, href = '', replace, scroll, shallow, ...other } = props
+  const { as, children, href = '', replace, scroll, shallow, ...other } = props
 
-  // Render as a regular `a` tag if external link.
-  if (external || /^https?:\/\//.test(href)) {
+  // Render as a regular `a` tag if href contains a protocol.
+  if (/^https?:\/\//.test(href)) {
     return (
       <a href={href} rel="noopener noreferrer" target="_blank" ref={ref} {...other}>
         {children}
