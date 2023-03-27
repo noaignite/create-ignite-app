c65659a70eb7f035bf1a046ddb57309396c6b6ee
adamsoderstrom <adamsoderstrom@me.com>
2023-03-27 23:20:51 +0200

    chore(Storybook): inline Babel config declaration in Storybook 'main' file
    Prior to this commit, we tried to only consume Babel when using Storybook. That was done by moving
    the 'babel.config.js' file to the '.storybook' directory. Though, when later updating to the
    latest Storybook 7 version, it does no longer work. Hence we strive for using the SWC for Next, we
    move the babel config declaration to Storybook's 'main' file.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,8 +2,54 @@ import { merge } from 'webpack-merge'
 import type { StorybookConfig } from '@storybook/nextjs'
 import webpackBaseConfig from '../webpackBaseConfig'
 
+const productionPlugins = ['@babel/plugin-transform-react-constant-elements']
+
 const config: StorybookConfig = {
   addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
+  babel: () => ({
+    presets: [
+      [
+        'next/babel',
+        {
+          // Target ES2015+ browsers
+          'preset-env': {
+            targets: 'Chrome >= 60, Safari >= 10.1, iOS >= 10.3, Firefox >= 54, Edge >= 15',
+            useBuiltIns: false,
+          },
+        },
+      ],
+    ],
+    plugins: [
+      'macros',
+      [
+        '@emotion',
+        {
+          // Allows an emotion component to be used as a CSS selector.
+          // https://github.com/mui/material-ui/issues/26366#issuecomment-942435579
+          importMap: {
+            '@mui/material': {
+              styled: {
+                canonicalImport: ['@emotion/styled', 'default'],
+                styledBaseImport: ['@mui/material', 'styled'],
+              },
+            },
+            '@mui/material/styles': {
+              styled: {
+                canonicalImport: ['@emotion/styled', 'default'],
+                styledBaseImport: ['@mui/material/styles', 'styled'],
+              },
+            },
+          },
+        },
+      ],
+    ],
+    env: {
+      development: {},
+      production: {
+        plugins: productionPlugins,
+      },
+    },
+  }),
   framework: {
     name: '@storybook/nextjs',
     options: {},
