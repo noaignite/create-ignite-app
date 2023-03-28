160794ff26f1515df6ca91fa9ebce665f7ad32f2
adamsoderstrom <adamsoderstrom@me.com>
2023-03-28 21:04:29 +0200

    chore(pages): remove old 'loadTranslations' function call

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import type { GetStaticPropsContext } from 'next'
 import * as blockPropGetters from '~/blocks/getBlockProps'
 import { createGetBlocksProps } from '~/utils'
-import loadTranslations from '~/api/i18n'
 import { pages, settings } from '~/api/__mock__'
 
 export { default } from '~/containers/Page'
@@ -12,12 +11,9 @@ export async function getStaticProps(context: GetStaticPropsContext) {
   const page = pages.Home
   const blocksWithData = page?.blocks ? await getBlocksProps(page.blocks, page, context) : []
 
-  const i18n = await loadTranslations(context.locale as string)
-
   return {
     props: {
       headerMode: 'auto',
-      i18n,
       defaultLocale: context.defaultLocale,
       locale: context.locale,
       settings,
