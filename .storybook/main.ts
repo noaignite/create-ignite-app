3de134964bccaebadea4c1e36f5352ffde43932b
adamsoderstrom <adamsoderstrom@me.com>
2023-03-28 19:54:38 +0200

    chore(babel): move babel config to root of repo
    This will result in 'babel' being used and not SWC, for the Next app.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,47 +4,6 @@ import webpackBaseConfig from '../webpackBaseConfig'
 
 const config: StorybookConfig = {
   addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
-  babel: () => ({
-    presets: [
-      [
-        'next/babel',
-        {
-          // Target ES2015+ browsers
-          'preset-env': {
-            targets: 'Chrome >= 60, Safari >= 10.1, iOS >= 10.3, Firefox >= 54, Edge >= 15',
-            useBuiltIns: false,
-          },
-        },
-      ],
-    ],
-    plugins: [
-      'macros',
-      [
-        '@emotion',
-        {
-          // Allows an emotion component to be used as a CSS selector.
-          // https://github.com/mui/material-ui/issues/26366#issuecomment-942435579
-          importMap: {
-            '@mui/material': {
-              styled: {
-                canonicalImport: ['@emotion/styled', 'default'],
-                styledBaseImport: ['@mui/material', 'styled'],
-              },
-            },
-            '@mui/material/styles': {
-              styled: {
-                canonicalImport: ['@emotion/styled', 'default'],
-                styledBaseImport: ['@mui/material/styles', 'styled'],
-              },
-            },
-          },
-        },
-      ],
-    ],
-    env: {
-      development: {},
-    },
-  }),
   framework: {
     name: '@storybook/nextjs',
     options: {},
