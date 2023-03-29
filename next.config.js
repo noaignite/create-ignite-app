1aae63c2c3d3d68ec118bf600598329f996ee93c
adamsoderstrom <adamsoderstrom@me.com>
2023-03-29 13:23:39 +0200

    chore(next.config): add 'webpack' property again

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
+const { merge } = require('webpack-merge')
+const webpackBaseConfig = require('./webpackBaseConfig')
 
 const nextConfig = {
   // For more on internalization see:
@@ -10,6 +12,7 @@ const nextConfig = {
     locales: ['en'],
   },
   poweredByHeader: false,
+  webpack: (config) => merge(config, webpackBaseConfig),
 }
 
 module.exports = withBundleAnalyzer(nextConfig)
