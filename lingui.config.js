41aac76359043360fc5f7688308fbe5319b33a3e
adamsoderstrom <adamsoderstrom@me.com>
2023-04-01 17:01:22 +0200

    chore(lingui): source locales from 'next.config.js'

--- lingui.config.js
+++ lingui.config.js
@@ -1,10 +1,12 @@
+const nextConfig = require('./next.config.js')
+
 /** @type {import('@lingui/conf').LinguiConfig} */
 module.exports = {
-  locales: ['en'],
+  locales: nextConfig.i18n.locales,
   extractBabelOptions: {
     presets: ['@babel/preset-typescript', '@babel/preset-react'],
   },
-  sourceLocale: 'en',
+  sourceLocale: nextConfig.i18n.defaultLocale,
   catalogs: [
     {
       path: 'public/locales/{locale}/messages',
