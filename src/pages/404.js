a51ef9b8de3744fb82bccd3a97b6096956824023
adamsoderstrom <adamsoderstrom@me.com>
2023-04-02 16:08:46 +0200

    chore(i18n): add lingui to '404.js'

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,10 @@
 import * as React from 'react'
 import Head from 'next/head'
+import { t } from '@lingui/macro'
 import { SITE_NAME } from '~/utils/constants'
-import { useI18n } from '~/contexts'
 import ErrorBlock from '~/blocks/ErrorBlock'
 
 function NotFoundPage() {
-  const { t } = useI18n()
-
   return (
     <React.Fragment>
       <Head>
@@ -15,11 +13,9 @@ function NotFoundPage() {
 
       <ErrorBlock
         subheading="404"
-        heading={t(__translationGroup)`Page not found`}
-        text={t(
-          __translationGroup,
-        )`The page you are looking for might have been renamed, removed or might never have existed.`}
-        ctaLabel={t(__translationGroup)`Back to homepage`}
+        heading={t`Page not found`}
+        text={t`The page you are looking for might have been renamed, removed or might never have existed.`}
+        ctaLabel={t`Back to homepage`}
         ctaUrl="/"
       />
     </React.Fragment>
